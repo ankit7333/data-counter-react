fef41ec8b10021199d7a03b9b92673a5739ba14b
ankitadulkar <ankit.adulkar1@justdial.com>
2024-03-08 14:45:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Counter />
   );
 }
 
-export default App;
+function Counter(){
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  let dayString;
+  if(count === 0){
+    dayString = `Today is `;
+  }else if(count > 0){
+    dayString = `${count} days from today is `;
+  }else if(count < 0){
+    dayString = `${count} days ago was `;
+  }
+
+  const handleReset = () => {
+    setStep(1);
+    setCount(0);
+  }
+  return (
+    <div>
+      <div>
+        <input type="range" min="0" max="10" value={step} onChange={(e) => setStep(Number(e.target.value))} />
+        <button onClick={() => setStep(currentStep => currentStep - 1)}>-</button>
+        <span>Step : {step}</span>
+        <button onClick={() => setStep(currentStep => currentStep + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount(currentCount => currentCount - step)}>-</button>
+        <span>Count : {count}</span>
+        <input type="number" value={count} onChange={(e) => setCount(Number(e.target.value))} />
+        <button onClick={() => setCount(currentCount => currentCount + step)}>+</button>
+        <p>
+          <span>{dayString}</span>
+          <span>{date.toDateString()}</span>
+          <span></span>
+        </p>
+        {(count !== 0 || step !== 1) ? (
+          <div>
+            <button onClick={handleReset}>Reset</button>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  )
+}
